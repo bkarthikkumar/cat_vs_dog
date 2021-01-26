4226700a03438f10df045d47a36495836f3d42fe
b-karthik-k <kumarkarthik.b20@gmail.com>
2021-01-26 18:09:36 +0530

    Code added for buton on home screen

--- lib/homeScree.dart
+++ lib/homeScree.dart
@@ -6,6 +6,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _loadingVar = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +16,7 @@ class _HomeState extends State<Home> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            SizedBox(height: 50),
+            SizedBox(height: 85),
             Text(
               "TeachableMachine.com CNN part",
               style: TextStyle(
@@ -38,6 +39,75 @@ class _HomeState extends State<Home> {
             SizedBox(
               height: 40,
             ),
+            Center(
+              child: _loadingVar
+                  ? Container(
+                      width: 260,
+                      child: Column(
+                        children: <Widget>[
+                          Image.asset('assets/cat.png'),
+                          SizedBox(
+                            height: 50,
+                          )
+                        ],
+                      ),
+                    )
+                  : Container(),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      width: MediaQuery.of(context).size.width - 150,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 25,
+                        vertical: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.yellow[900],
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Text(
+                        'Take a photo',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black,
+                            fontSize: 20),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      width: MediaQuery.of(context).size.width - 150,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 25,
+                        vertical: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.red[900],
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Text(
+                        'Camera Roll',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 20),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
