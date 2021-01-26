fcd0d0e3c8290fe31c02b70fe54f384b9d490cab
b-karthik-k <kumarkarthik.b20@gmail.com>
2021-01-26 16:56:48 +0530

    Code added for homescreen dart file , spalshscreen dartFile, added asset folder and image for background on 26 Jan 2020

---
+++ lib/splashscreen.dart
@@ -0,0 +1,32 @@
+import 'package:cat_vs_dog/homeScree.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class MySpalshScreen extends StatefulWidget {
+  @override
+  _MySpalshScreenState createState() => _MySpalshScreenState();
+}
+
+class _MySpalshScreenState extends State<MySpalshScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 2,
+      navigateAfterSeconds: Home(),
+      title: Text(
+        'Dog and Cat Spalsh',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 30,
+          color: Color(0xFFE99600),
+        ),
+      ),
+      image: Image.asset(
+        'assets/cat.png',
+      ),
+      backgroundColor: Colors.black,
+      photoSize: 50,
+      loaderColor: Color(0xFFEDA28),
+    );
+  }
+}
